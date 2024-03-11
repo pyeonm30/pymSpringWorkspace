80b45987e08cfb19d7fabb45d7b09f8ecc53b7ae
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-11 17:21:34 +0900

    step01
    .

---
+++ 01_spring_basic/src/main/java/com/myspring/test/HomeController.java
@@ -0,0 +1,58 @@
+package com.myspring.test;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+
+	_01Member test1 = new _01Member();
+	
+	@Autowired
+	_01Member test2;   // _01Member test2 = member;
+	
+	@Autowired
+	_01Member test3;  // _01Member test2 = member;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+
+		
+		System.out.println(">> 1단계 >>  ");
+		System.out.println("test1의 주소 = " + test1);
+		test1.setNum(100);
+		test1.getNum();
+		
+		System.out.println("test2의 주소 = " + test2);
+		System.out.println("test2의 주소 = " + test3);
+
+		test2.setNum(300);
+		
+		test2.getNum();
+		test3.getNum();
+		
+		
+		return "home";  //"/WEB-INF/"+home+".jsp";
+	}
+	
+}
+
+
+
+
+
+
+
---
+++ 01_spring_basic/src/main/java/com/myspring/test/_01Member.java
@@ -0,0 +1,16 @@
+package com.myspring.test;
+
+public class _01Member {
+	private int num;
+
+	public int getNum() {
+		System.out.println( "num = " + num);
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+
+}
