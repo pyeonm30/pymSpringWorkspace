d3ba254740d70e6f485f2b1be8225b88523ef855
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-13 16:27:20 +0900

    .
    .

---
+++ BoardMVC02/src/main/java/kr/board/controller/MainController.java
@@ -0,0 +1,14 @@
+package kr.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";  // /WEB-INF/views/ + home +".jsp
+	}
+
+}
