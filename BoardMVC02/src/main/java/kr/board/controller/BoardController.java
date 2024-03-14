b75e42edd5516caae3bffc634e63998a6bb1b9f5
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-14 14:27:53 +0900

    .
    .

--- BoardMVC02/src/main/java/kr/board/controller/BoardController.java
+++ BoardMVC02/src/main/java/kr/board/controller/BoardController.java
@@ -1,5 +1,37 @@
 package kr.board.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.board.entity.Board;
+import kr.board.mapper.BoardMapper;
+//@Controller --> 리턴을 뷰리졸버로 넘김 
+@RequestMapping("/api")
+@RestController    // 리턴값을 @ResponseBody 넘겨줌 
 public class BoardController {
 
+	@Autowired
+	BoardMapper boardMapper;
+	
+	@GetMapping("/boards")
+	public List<Board> boardList(){
+		List<Board> list = boardMapper.getLists();
+		return list;
+	}
+	
+	                           // json 요청을 줄때는 @RequestBody 넣어야함!! 
+	@PostMapping("/boards")   //  Board 객체 --> @ModelAttribute(기본으로 적용되기때문에 )
+	public String addBoard(@RequestBody Board board) {
+		String msg =board.toString();
+		
+		
+		return msg;
+	}
+	
 }
--- BoardMVC02/src/main/java/kr/board/mapper/BoardMapper.java
+++ BoardMVC02/src/main/java/kr/board/mapper/BoardMapper.java
@@ -1,8 +1,12 @@
 package kr.board.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import kr.board.entity.Board;
 
 @Mapper  // 기본생성자 -> setter()
 public interface BoardMapper {
-
-}
+	public List<Board> getLists();
+ }
