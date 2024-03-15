cd853a1fafb4a3d9a2322ab2c361c85f3f18372f
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-15 16:42:16 +0900

    .
    .

--- BoardMVC6/src/main/java/kr/board/config/SecurityConfig.java
+++ BoardMVC6/src/main/java/kr/board/config/SecurityConfig.java
@@ -2,18 +2,33 @@ package kr.board.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import kr.board.security.MemberUserDetailsService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Bean
+	public UserDetailsService memberUserDetailsService() {
+		return new MemberUserDetailsService();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(memberUserDetailsService())
+		.passwordEncoder(passwordEncoder());
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		//요청에대한 보안 설정~~
